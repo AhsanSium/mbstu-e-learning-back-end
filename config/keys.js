652f529a131bb64ff00f61b9c17f9f27e6f52a5f
Ahsan Sium <ahsansium28@gmail.com>
2023-03-11 03:06:33 +0600

    Feature Update

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
   //mongoURI: "mongodb://spectre:spectre007@ds253537.mlab.com:53537/elearning", //apurva's
-  mongoURI: 'mongodb+srv://admin:admin@cluster0-xb8vd.gcp.mongodb.net/E-learning?retryWrites=true&w=majority',
+  mongoURI: 'mongodb+srv://mbstu-e-learning-project-039:9Bv2Ug8GVrv1QxxE@cluster0.afifr.mongodb.net/mbstu-e-learning-project-039?retryWrites=true&w=majority',
   secretOrKey: "secret"
 };
